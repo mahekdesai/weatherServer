9e7b4bc4181ddb202148eaf8bacab90022f00586
mahekdesai <mahekdesai2001@gmail.com>
2024-03-08 20:46:39 -0800

    fifth

--- CountryModel/City.cs
+++ CountryModel/City.cs
@@ -20,6 +20,10 @@ public partial class City
 
     public int CountryId { get; set; }
 
+    public required string Name { get; set; }
+
+    public int Population { get; set; }
+
     [ForeignKey("CountryId")]
     [InverseProperty("Cities")]
     public virtual Country Country { get; set; } = null!;
