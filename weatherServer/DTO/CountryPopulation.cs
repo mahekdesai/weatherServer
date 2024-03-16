25f3374190c9b3fc87f7464911cc4f0543e144f4
mahekdesai <mahekdesai2001@gmail.com>
2024-03-15 20:45:46 -0700

    sixth

---
+++ weatherServer/DTO/CountryPopulation.cs
@@ -0,0 +1,9 @@
+﻿namespace weatherServer.DTO
+{
+    public class CountryPopulation
+    {
+        public required string Name { get; set; }
+        public int CountryId { get; set; }
+        public int Population { get; set; }
+    }
+}
